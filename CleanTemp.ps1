3844921e74753606f57f974494abf4e1c6ffe369
catshaker <76237246+catshaker@users.noreply.github.com>
2021-05-19 22:22:45 -0400

    Add files via upload

---
+++ CleanTemp.ps1
@@ -0,0 +1,6 @@
+﻿get-childitem $env:tmp -Recurse |
+Remove-Item -recurse -Force -erroraction SilentlyContinue
+
+get-childitem C:\WINDOWS\Temp -recurse |
+Remove-Item -recurse -Force -erroraction SilentlyContinue
+
